204d8a8c90a77ffa9927dc84b38b3f84230acfac
Bryle James Agra <brfe.agra.up@phinmaed.com>
2026-08-05 01:40:29 +0800

    feat: enable offline support by adding vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
@@ -10,7 +11,18 @@ export default defineConfig({
       overlay: false,
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "favicon.svg"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,mp4,jpg,jpeg,webp,woff2}"],
+        maximumFileSizeToCacheInBytes: 150000000,
+      },
+      manifest: false, // We're using the existing site.webmanifest in index.html
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
